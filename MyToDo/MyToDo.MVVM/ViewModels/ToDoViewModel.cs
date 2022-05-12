24db00cff0f3697a8515c1cb146390aea8a2af00
洛洛 <lolo123777@outlook.com>
2022-05-12 14:35:38 +0800

    wpf todoView的创建

--- MyToDo/MyToDo.MVVM/ViewModels/ToDoViewModel.cs
+++ MyToDo/MyToDo.MVVM/ViewModels/ToDoViewModel.cs
@@ -4,4 +4,27 @@ public class ToDoViewModel : ViewModelBase, IRoutableViewModel
 {
     public string? UrlPathSegment { get; }
     public IScreen HostScreen { get; }
+    public ObservableCollection<ToDoDto> ToDoDtos { get; set; } = new();
+    [Reactive] public bool IsDrawerHostAddToDoIsOpen { get; set; }
+    public ReactiveCommand<Unit, bool> AddToDoCommand { get; set; }
+
+    protected override void SetupStart()
+    {
+        base.SetupStart();
+        CreatTestData();
+    }
+
+    protected override void SetupCommands()
+    {
+        base.SetupCommands();
+        AddToDoCommand = ReactiveCommand.Create(() => IsDrawerHostAddToDoIsOpen = true);
+    }
+
+    private void CreatTestData()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            ToDoDtos.Add(new ToDoDto { Title = "ToDo" + i, Content = "待处理中。。。" });
+        }
+    }
 }
--- MyToDo/MyToDo.Wpf/Views/ToDoView.xaml.cs
+++ MyToDo/MyToDo.Wpf/Views/ToDoView.xaml.cs
@@ -10,6 +10,9 @@ public partial class ToDoView
         InitializeComponent();
         this.WhenActivated(d =>
         {
+            this.OneWayBind(ViewModel, vm => vm.ToDoDtos, v => v.itemControlToDo.ItemsSource).DisposeWith(d);
+            this.BindCommand(ViewModel, vm => vm.AddToDoCommand, v => v.btnAddToDo).DisposeWith(d);
+            this.Bind(ViewModel, vm => vm.IsDrawerHostAddToDoIsOpen, v => v.drawerRightAddToDo.IsRightDrawerOpen).DisposeWith(d);
         });
     }
 }
